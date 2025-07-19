5572bbab90a4f7b3c1f1485409b769443cbb44cb
Adrian Moring <adriantmoringbass@gmail.com>
2025-07-19 11:53:20 -0400

    Almost got it but still failing the over 100 perecnt last function test

--- index.js
+++ index.js
@@ -5,3 +5,65 @@
 
 // This is required for the test to function properly  
 module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+
+function calculateTax(amount){
+
+    taxValue = amount * 0.10
+    return taxValue
+}
+
+function convertToUpperCase(text){
+
+    upperCaseText = text.toUpperCase();
+    return upperCaseText
+}
+
+function findMaximum(num1, num2){
+
+    if (num1 > num2){
+
+        return num1
+    }
+    else if(num1 === num2){
+
+        return num1
+    }
+    else{
+
+        return num2
+    }
+}
+
+function isPalindrome(word){
+
+    if (word === word.split('').reverse().join('')){
+
+        return true
+    }
+    else{
+
+        return false
+    }
+}
+
+function calculateDiscountedPrice(originalPrice, discountPercentage){
+
+    if (discountPercentage === 100 ){
+
+        const discountedPrice = 0
+    }
+
+    //else if(discountPercentage > 100){
+
+        //console.log("discountPercentage only up to 100%")
+    //}
+
+    else{
+
+    discountDecimal = discountPercentage/100
+
+    discountedPrice = originalPrice - (originalPrice * discountDecimal)
+
+    return discountedPrice
+}
+}
